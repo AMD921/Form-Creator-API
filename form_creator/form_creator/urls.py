951e38152d5825a36f8c34dcd356abc127b441a3
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-10-17 14:45:46 +0330

    install DRF

--- form_creator/form_creator/urls.py
+++ form_creator/form_creator/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
