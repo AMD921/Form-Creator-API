270b0ef435c3c738099ca1de644e251c5aefeb4b
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-10-19 20:00:04 +0330

    feat: add urls

--- form_creator/form_creator/urls.py
+++ form_creator/form_creator/urls.py
@@ -22,4 +22,5 @@ from django.urls import include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('report/', include('report.urls')),
 ]
