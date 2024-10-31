3141bb9216ea10fdacbcb934d5affc2eab7f095b
Mahan MI(PC) <majlesi.mahan@gmail.com>
2024-10-31 12:18:18 +0330

    feat: integrate environment variables for SECRET_KEY and DEBUG settings

--- form_creator/form_creator/settings.py
+++ form_creator/form_creator/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 from celery.schedules import crontab
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+)3l556z(tt4_+1#qj46(#tp0ff29@re*sg*pxz9n(9!+ndc3i'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
